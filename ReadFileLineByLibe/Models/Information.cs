88470942c2dccc871adcddccc087b755f3beeb21
bdshanto <hasib@nybsys.com>
2023-01-18 20:48:52 +0600

    Initla commit

---
+++ ReadFileLineByLibe/Models/Information.cs
@@ -0,0 +1,11 @@
+﻿namespace ReadFileLineByLibe.Models;
+
+public class Information
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string MiddleName { get; set; }
+    public string LastName { get; set; }
+    public string Location { get; set; }
+    public string ContactNo { get; set; }
+}
--- ReadFileLineByLibe/Program.cs
+++ ReadFileLineByLibe/Program.cs
@@ -1,13 +1,62 @@
 ﻿using System;
 using System.IO;
+using System.Reflection;
+using ReadFileLineByLibe.Models;
 
 public class ReadFileLineByLine
 {
     public static void Main()
     {
-        string fileName = @"D:\Test.txt";
+        /*Find the file path from the data folder.*/
+        var path = Path.Combine(Environment.CurrentDirectory, "Data", "Test.txt");
 
-        string[] lines = File.ReadAllLines(fileName);
-        Console.WriteLine(String.Join(Environment.NewLine, lines));
+        /*Read the all lines from the file.*/
+        string[] lines = File.ReadAllLines(path);
+
+        /*
+         * Assume a model according the depend on Text file data/
+         * Here i assume a model which name is information.
+         */
+
+        /*Since i know the file has multiple rows,
+         *So i create a list in 24 no line.
+         */
+        var information = new List<Information>();
+
+
+        /*
+         *Here i use a loop for each rows
+         */
+        foreach (var line in lines)
+        {
+            /*
+             * Split the every column of the rows.
+             */
+            var lineParts = line.Split(" ");
+
+            /*
+             * Add information into the list.
+             */
+            information.Add(new Information()
+            {
+                Id = Convert.ToInt32(lineParts[0]),
+                FirstName = lineParts[1],
+                MiddleName = lineParts[2],
+                LastName = lineParts[3],
+                Location = lineParts[4],
+                ContactNo = lineParts[5],
+            });
+        }
+
+        /*
+         * Here is the final result
+         */
+        Console.WriteLine(@"The whole output file are generated from \data\test.txt file");
+        foreach (var info in information)
+        {
+
+            Console.WriteLine($"SL:{info.Id} Name: {info.FirstName} {info.MiddleName} {info.LastName} Location: {info.Location} Contact: {info.ContactNo}");
+        }
+
     }
 }
