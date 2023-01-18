845d9413e64f4a0669bce1181812a0a5609634df
bdshanto <hasib@nybsys.com>
2023-01-18 20:00:01 +0600

    Add project files.

---
+++ ReadFileLineByLibe/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+public class ReadFileLineByLine
+{
+    public static void Main()
+    {
+        string fileName = @"D:\Test.txt";
+
+        string[] lines = File.ReadAllLines(fileName);
+        Console.WriteLine(String.Join(Environment.NewLine, lines));
+    }
+}
